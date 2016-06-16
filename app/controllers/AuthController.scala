57030e5cdfcb6032ab216a6d90600977f8a47cc8
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-06-16 02:02:06 +0100

    added Auth controller and index view

---
+++ app/controllers/AuthController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class AuthController @Inject() extends Controller {
+
+  val spotify = SpotifyController.getInstance
+
+  def auth = Action { Redirect(spotify.getAuthorizeURL) }
+
+  /*
+  play.api.mvc.Action type is a wrapper
+  around the type `Request[A] => Result`,
+  where `Request[A]` identifies an HTTP request
+  and `Result` is an HTTP response.
+  */
+  def callback = Action {
+    request => request.getQueryString("code") match {
+      case Some(code) =>
+        spotify.getAccessToken(code)
+        Ok(views.html.callback("Welcome, " + spotify.getName))
+      case None => BadRequest("Something went wrong! Please go back and try again.")
+    }
+    /**
+      * TODO
+      * is badRequest a proper return code for each?
+      * if VALUES: [access_denied] return badRequest(index.render(...));
+      * else (if VALUES: [invalid_scope] or something else)
+      * return badRequest(callback.render(map.get("error")[0]));
+      */
+  }
+
+}
