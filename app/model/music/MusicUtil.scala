d8cbae9a48d1ff3efb2e640d8c05b58e0f249cd7
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-07-29 15:54:24 +0100

    parse ajax json request

--- app/model/music/MusicUtil.scala
+++ app/model/music/MusicUtil.scala
@@ -33,4 +33,107 @@ object MusicUtil {
     ))
   }
 
+  def parseIDS(js: JsValue): Vector[String] = {
+    // checking ids(TODO move to its own method)
+    val ids = js \ "ids"
+    // TODO you might get rid of this
+    if (ids.isInstanceOf[JsUndefined]) {
+      // no songs selected, should get 20 random/'n' if constraint is given from user db
+      println("======> NO SONGS SELECTED")
+      Vector()
+    } else {
+      (ids \\ "id").map(js => js.as[String]).toVector
+    }
+  }
+
+  // TODO could change json format with key type for indexed, standard etc
+  // again reflection todo
+  /**
+    * parse json of the form
+    * { "name": 'playlistName', "ids": [{"id": 'id1'}, {"id": 'id2'}],
+    *   "constraints": [{"constraint": [{"name": 'c_name', "attribute": 'c_attribute'}]}]
+    * TODO do later
+    *
+    * @param js
+    * @return
+    */
+  def parseRequest(js: JsValue): (String, Set[Constraint]) = {
+    val name = (js \ "name").as[String]
+    // checking constraints(TODO move to its own method)
+    if((js \ "constraints").isInstanceOf[JsUndefined]) {
+      //      println("======> CONSTRAINTS UNDEFINED")
+      (name, Set())
+    }
+    else {
+      (name,
+        (js \ "constraints" \\ "constraint").map(c => (c \ "name").as[String] match {
+          case "Include" => Include((c \ "index").as[Int], parseJsonAttribute((c \ "attribute").get))
+          case "Exclude" => Exclude((c \ "index").as[Int], parseJsonAttribute((c \ "attribute").get))
+          case "IncludeSmaller" => IncludeSmaller((c \ "index").as[Int],
+            parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "IncludeLarger" => IncludeLarger((c \ "index").as[Int],
+            parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+
+          //     case "ExcludeAll" => ExcludeAll(parseJsonAttribute((c \ "attribute").get))
+
+          case "UnaryLargerAll" => UnaryLargerAll(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "UnaryLargerAny" => UnaryLargerAny(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "UnaryLargerNone" => UnaryLargerNone(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "UnarySmallerAll" => UnarySmallerAll(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "UnarySmallerAny" => UnarySmallerAny(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+          case "UnarySmallerNone" => UnarySmallerNone(parseJsonAttribute((c \ "attribute").get).asInstanceOf[AudioAttribute])
+
+          case unknown => throw new Exception(unknown + ": constraint not found")
+        }).toSet)
+    }
+  }
+
+  def parseIndexedConstraint(constraint: (String, Int, List[String])): Constraint = constraint match {
+    //    case ("Exclude", index, attr) => Exclude(index, parseAttribute(attr))
+    //    case ("Include", index, attr) => Include(index, parseAttribute(attr))
+    case (name, _, _) => throw new Exception(name + ": constraint not found")
+  }
+
+  // TODO: REFLECTION, or at least intercept TimeAttribute to assign Double etc.
+  def parseJsonAttribute(js: JsValue): Attribute = {
+    val value = (js \ "value").as[String]
+    (js \ "name").as[String] match {
+      case "Acousticness" => Acousticness(value.toDouble)
+      case "Duration" =>
+        val tuple = value.split(":")
+        val result = (tuple(0).toDouble * 60000) + (tuple(1).toDouble * 1000)
+        println("PARSED " + value + " as " + result)
+        Duration(result)
+      case "Loudness" => Loudness(value.toDouble)
+      case "Tempo" => Tempo(value.toDouble)
+      case "Year" => Year(value.toInt)
+      case unknown => throw new Exception(unknown + ": attribute not found")
+    }
+  }
+
+  // TODO: REFLECTION
+  // this approach has less JavaScript parsing and input overhead
+  def parseAttribute(json: List[String]): Attribute = json match {
+    case name :: value => name match {
+      case "Acousticness" => value match {
+        case v :: Nil => Acousticness(v.toDouble)
+        case _ => throw new Exception("Acousticness has a bad format")
+      }
+      case "Artist" => value match {
+        case v :: Nil => Artist(v)
+        case _ => throw new Exception("Artist has a bad format")
+      }
+      case "Year" => value match {
+        case v :: Nil => Year(v.toInt)
+        case _ => throw new Exception("Year has a bad format")
+      }
+      case "Duration" => value match {
+        case v :: Nil => Duration(v.toInt)
+        case _ => throw new Exception("Duration has a bad format")
+      }
+    }
+    case _ => throw new Exception("Attribute has a bad format")
+  }
+
+
 }
