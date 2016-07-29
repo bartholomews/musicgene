f116c4eaa4abd930c6d47bb0553c050d09a8080b
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-07-29 15:55:03 +0100

    first version, id + Set[Attribute]

---
+++ app/model/music/Song.scala
@@ -0,0 +1,13 @@
+package model.music
+
+import java.util.concurrent.TimeUnit
+
+import controllers.SpotifyController
+
+/**
+  *
+  */
+class Song(val id: String, val attributes: Set[Attribute]) {
+
+
+}
