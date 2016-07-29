3f0fc616af4a991e48c9075e99a2824af5fcef45
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-07-29 15:55:27 +0100

    val instances to feed the view

--- app/model/music/Song.scala
+++ app/model/music/Song.scala
@@ -9,5 +9,37 @@ import controllers.SpotifyController
   */
 class Song(val id: String, val attributes: Set[Attribute]) {
 
+  def getAttribute(that: Attribute): Option[Attribute] = attributes.find(a => a.getClass == that.getClass)
 
+  def find(that: Attribute): String = getAttribute(that) match {
+    case None => "[unknown]"
+    case Some(a) => a.value.toString
+  }
+
+  val duration: Double = getAttribute(Duration(0)) match {
+    case None => 0
+    case Some(d) => d.value.asInstanceOf[Double]
+  }
+
+  val durationToString = {
+    val seconds: Int = ((duration / 1000) % 60).toInt
+    val minutes = ((duration / (1000*60)) % 60).toInt
+    "%02d".format(minutes) + ":" + "%02d".format(seconds)
+  }
+
+  /*
+    Ok for view display, but it's not nice
+    and it will throw an exception if used
+    at the back end if the attribute is not found
+    ("unknown" parsed to another data type),
+    should be using Option[value] for that.
+   */
+  val title = find(Title(""))
+  val artist = find(Artist(""))
+  val album = find(Album(""))
+  val tempo = find(Tempo(0))
+  val loudness = find(Loudness(0))
+  val acousticness = find(Acousticness(0))
+
+  object Song
 }
