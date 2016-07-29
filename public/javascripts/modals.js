be9c708f2306622ea160a1d9f5ecc3530d44f4a9
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-07-29 15:46:30 +0100

    modals functionality, basic constraints

--- public/javascripts/modals.js
+++ public/javascripts/modals.js
@@ -1,4 +1,38 @@
 /**
+ * TODO clear input modals when either 'undo' or 'save' button pressed
+ */
+$(document).ready(function () {
+    playPreview();  // listen for mouse click on track rows
+    resetModal(); // listen for a closed modal
+    checkSubmitModal();
+});
+
+/**
+ * When an 'equals' or 'contains' button is clicked, its input text box is enabled
+ * and on focus. TODO
+ */
+function enableInput(inputID) {
+    var input = document.getElementById(inputID);
+    input.disabled = false;
+    input.focus();
+}
+
+/**
+ * On each input box in a modal,
+ * if 'Enter' is pressed the 'Save' button will be triggered.
+ * ----------
+ * reference:
+ * {@link http://stackoverflow.com/a/155265}
+ */
+function isEnterKeyPressed(e, id) {
+    if (e.keyCode == 13) {  // 'Enter' is pressed
+        document.getElementById(id).click();
+        return false;
+    }
+    return true;
+}
+
+/**
  *
  */
 function checkSubmitModal() {
@@ -42,4 +76,33 @@ function saveModal(name) {
  e.preventDefault();
  $("#wrapper").toggleClass("toggled");
  });
+*/
+
+/*
+ function initSwitches() {
+ $("[name='radio-quantity']").bootstrapSwitch();
+ }
+*/
+
+/**
+ * @see http://stackoverflow.com/a/1953093
  */
+function checkAttributeFireSlider() {
+    $("#attr").change(function () {
+        var id = $(this).find("option:selected").attr("id");
+        var sliderDiv = document.getElementById('wrapper-attr-slider');
+        switch (id) {
+            case "attr-year":
+                console.log("ATTR_YEAR SELECTED");
+                sliderDiv.style.visibility = 'visible';
+                $("#attr-slider").slider({});
+                break;
+            case "attr-duration":
+                console.log("ATTR_DURATION SELECTED");
+                break;
+            case "attr-tempo":
+                console.log("ATTR_TEMPO SELECTED");
+                break;
+        }
+    });
+}
