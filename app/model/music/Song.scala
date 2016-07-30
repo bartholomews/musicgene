4f55087dfc08b4dfb4bf765470ce239b8bd16cbc
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-07-30 05:03:23 +0100

    added string representation of attributes

--- app/model/music/Song.scala
+++ app/model/music/Song.scala
@@ -11,29 +11,32 @@ class Song(val id: String, val preview_url: String, val attributes: Set[Attribut
 
   def getAttribute(that: Attribute): Option[Attribute] = attributes.find(a => a.getClass == that.getClass)
 
-  def find(that: Attribute): String = getAttribute(that) match {
-    case None => "[unknown]"
-    case Some(a) => a.value.toString
-  }
-
   val duration: Double = getAttribute(Duration(0)) match {
     case None => 0
     case Some(d) => d.value.asInstanceOf[Double]
   }
 
+  def find(that: Attribute): String = getAttribute(that) match {
+    case None => "[unknown]"
+    case Some(a) => a.value.toString
+  }
+
+  // =============================================================================
+  // STRING VALS
+  // =============================================================================
+  /*
+  Ok for view display, but it's not nice
+  and it will throw an exception if used
+  at the back end if the attribute is not found
+  ("unknown" parsed to another data type),
+  should be using Option[value] for that.
+ */
   val durationToString = {
     val seconds: Int = ((duration / 1000) % 60).toInt
     val minutes = ((duration / (1000*60)) % 60).toInt
     "%02d".format(minutes) + ":" + "%02d".format(seconds)
   }
 
-  /*
-    Ok for view display, but it's not nice
-    and it will throw an exception if used
-    at the back end if the attribute is not found
-    ("unknown" parsed to another data type),
-    should be using Option[value] for that.
-   */
   val title = find(Title(""))
   val artist = find(Artist(""))
   val album = find(Album(""))
