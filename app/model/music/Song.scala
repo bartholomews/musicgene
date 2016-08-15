28c2e0f30edac607cff99a635cdeb277135b91a2
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-08-15 04:13:16 +0100

    getOrElse, findValue defs

--- app/model/music/Song.scala
+++ app/model/music/Song.scala
@@ -11,11 +11,32 @@ import controllers.SpotifyJavaController
 trait Song {
   val id: String
   val attributes: Set[Attribute]
+
+  /*
+  def find(that: Attribute): Option[Attribute] = attributes.find(a => a.getClass == that.getClass) match {
+    case None => None
+    case Some(a) => Some(a)
+  }
+  */
+
   def getOrElse(that: Attribute): Attribute = attributes.find(a => a.getClass == that.getClass) match {
     case None => that
     case Some(a) => a
   }
-  def find(that: Attribute): String = getOrElse(that).value.toString
+
+  def getOrElse(that: Attribute, placeholder: String): String = attributes.find(a => a.getClass == that.getClass) match {
+    case None => placeholder
+    case Some(a) => a.value.toString
+  }
+
+  // should rename that
+  def findValue(that: Attribute): Option[String] = attributes.find(a => a.getClass == that.getClass) match {
+    case None => None
+    case Some(a) => Some(a.value.toString)
+  }
+
+  def find(that: Attribute) = findValue(that).getOrElse(that.value.toString)
+
 }
 
 case class SpotifySong(id: String, attributes: Set[Attribute]) extends Song {
@@ -38,24 +59,24 @@ case class SpotifySong(id: String, attributes: Set[Attribute]) extends Song {
     "%02d".format(minutes) + ":" + "%02d".format(seconds)
   }
 
-  val title = find(Title(""))
-  val preview_url = find(Preview_URL(""))
-  val artist = find(Artist(""))
-  val album = find(Album(""))
-  val tempo = find(Tempo(0))
-  val loudness = find(Loudness(0))
+  val title = findValue(Title(""))
+  val preview_url = findValue(Preview_URL(""))
+  val artist = findValue(Artist(""))
+  val album = findValue(Album(""))
+  val tempo = findValue(Tempo(0))
+  val loudness = findValue(Loudness(0))
 
   /**
     * The more suitable for dancing,
     * the closer to 1.0 value.
     * @see http://developer.echonest.com/acoustic-attributes.html
     */
-  val danceability = find(Danceability(0))
+  val danceability = findValue(Danceability(0))
   /**
     * Voice and acoustic instruments closer to 0
     * Synthesizers, amplifiers, distortions, etc. closer to 1
     */
-  val acousticness = find(Acousticness(0))
+  val acousticness = findValue(Acousticness(0))
   /**
     * The more exclusively speech-like the closer to 1.0
     * Above 0.66 tracks are probably made
@@ -63,18 +84,16 @@ case class SpotifySong(id: String, attributes: Set[Attribute]) extends Song {
     * Between 0.33 and 0.66 might contain both music and speech (e.g. rap)
     * Below 0.33 most likely music
     */
-  val speechiness = find(Speechiness(0))
+  val speechiness = findValue(Speechiness(0))
   /**
     *  The more confident the track is live,
     *  the closer to 1.0
     *  Above 0.8 strong likelihood for live track,
     *  below 0.6 most likely studio recordings
     */
-  val liveness = find(Liveness(0))
+  val liveness = findValue(Liveness(0))
 
-  val valence = find(Valence(0))
-  val energy = find(Energy(0))
+  val valence = findValue(Valence(0))
+  val energy = findValue(Energy(0))
 
-
-  object Song
 }
