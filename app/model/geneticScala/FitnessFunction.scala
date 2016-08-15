beefb25a4d7d06ca2500e7a37777035aa10a2287
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-08-15 04:14:02 +0100

    BigDecimal setScale on distance

--- app/model/geneticScala/FitnessFunction.scala
+++ app/model/geneticScala/FitnessFunction.scala
@@ -1,6 +1,6 @@
 package model.geneticScala
 
-import model.constraints.{Constraint, Score, ScoreConstraint, UnaryConstraint}
+import model.constraints.{Constraint, Score, UnaryConstraint}
 
 
 /**
@@ -18,6 +18,7 @@ trait FitnessFunction {
 /**
 * Created by mba13 on 24/07/2016.
 */
+/*
 case class StandardFitness(constraints: Set[UnaryConstraint]) extends FitnessFunction {
 
   override def getConstraints: Set[Constraint] = constraints.asInstanceOf[Set[Constraint]]
@@ -32,17 +33,23 @@ case class StandardFitness(constraints: Set[UnaryConstraint]) extends FitnessFun
     * TODO some points for partially successful ones?
     * how to deal with constraint relative to relationship between tracks?
     *
-    * @param playlist the Playlist to be tested against the constraints
     * @return
     * NaN for 0.0/0.0
     */
+  /*
   override def getFitness(playlist: Playlist): Double = {
-    val f = constraints.count(constraint => constraint.calc(playlist)) / constraints.size.toDouble
-    BigDecimal.decimal(f).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val f1 = constraints.count(constraint => constraint.score(playlist))
+    val f2 = constraints.size.toDouble
+    val f = constraints.count(constraint => constraint.score(playlist)) / constraints.size.toDouble
+    val r = BigDecimal.decimal(f).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    println(f1 + " / " + f2 + " = " + r)
+    r
   }
-}
+  */
+  * }
+  * */
 
-case class CostBasedFitness(constraints: Set[ScoreConstraint]) extends FitnessFunction {
+case class CostBasedFitness(constraints: Set[Constraint]) extends FitnessFunction {
   override def getConstraints: Set[Constraint] = constraints.asInstanceOf[Set[Constraint]]
 
   override def score(p: Playlist): Set[Score] = constraints.flatMap(c => c.score(p))
@@ -50,12 +57,16 @@ case class CostBasedFitness(constraints: Set[ScoreConstraint]) extends FitnessFu
   override def getFitness(p: Playlist): Double = {
     if(constraints.isEmpty) 0.0
     else {
+      val f1 = score(p).count(s => s.matched)
+      val f2 = score(p).size.toDouble
       val f = score(p).count(s => s.matched) / score(p).size.toDouble
       BigDecimal.decimal(f).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  //    println(f1 + " / " + f2 + " = " + r + " (distance: " + getDistance(p) + ")")
     }
   }
   override def getDistance(p: Playlist): Double = {
-    score(p).map(s => s.distance).sum
+    val distance = score(p).map(s => s.distance).sum
+    BigDecimal.decimal(distance).setScale(4, BigDecimal.RoundingMode.HALF_UP).toDouble
   }
 }
 
