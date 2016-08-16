418db12fe0d45b21473a6129ec0b10a186e46a23
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-08-16 16:40:54 +0100

    removed old solutions commented out

--- app/model/constraints/ConstraintsUtil.scala
+++ app/model/constraints/ConstraintsUtil.scala
@@ -96,38 +96,3 @@ object ConstraintsUtil {
   }
 
 }
-
-
-  /*
-  // this MIGHT work for equals f, but what about > and < ?
-  def compareScore(s: Song, that: AudioAttribute, tolerance: Double, f: (Double, Double) => Boolean): Score = {
-    s.attributes.find(a => a.getClass == that.getClass) match {
-      case None => Score(matched = false, 1)  // 1 to low, could be even a good score
-      case Some(attr) => attr.value match {
-        case x: Number => calculateScoreEquals(x.asInstanceOf[Double], that.value, tolerance, f)
-        case z => throw new Exception(z + ": " + z.getClass + " is not a java.lang.Number")
-      }
-    }
-  }
-
-  // nononononno
-  private def calculateScoreEquals(x: Double, y: Double, tolerance: Double, f: (Double, Double) => Boolean) = {
-    val cost = scala.math.abs(x - y); val fitness = tolerance - cost
-    if(fitness >= 0) Score(matched = true, cost)
-    else Score(matched = false, cost)
-  }
-
-  // ScoreGreater should have a worst fitness the more is greater than the value, and be double that bad going other way
-  // so that a 1.2 > 1.1 should be the best match, 1.3 > 1.1 should have cost 0.1; 0.8 > 1.0 false(0.3)
-  private def calculateScoreGreatLess(x: Double, y: Double, f: (Double, Double) => Boolean) = {
-    val fitness =
-    if(f(x,y)) Score(matched = false, y + 0.1 - x)
-    else Score(matched = true, y - x + 0.1)
-
-  }
-
-  // cost = 2.2
-
-}
-
-*/
