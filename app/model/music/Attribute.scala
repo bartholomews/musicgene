d61f69d13a40cf1cece5539526854467ef3d136c
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-01 03:26:47 +0100

    PercentageAttribute for min/max values

--- app/model/music/Attribute.scala
+++ app/model/music/Attribute.scala
@@ -15,10 +15,23 @@ trait TimeAttribute extends Attribute {
   override def value: Int
 }
 trait AudioAttribute extends Attribute {
-  val min: Double = 0.0
-  val max: Double = 1.0
   override def value: Double
+  val max: Double
+  val min: Double
 }
+
+trait PercentageAttribute extends AudioAttribute {
+  override val min: Double = 0.0
+  override val max: Double = 1.0
+}
+
+case class Acousticness(value: Double) extends PercentageAttribute
+case class Danceability(value: Double) extends PercentageAttribute
+case class Energy(value: Double) extends PercentageAttribute
+case class Instrumentalness(value: Double) extends PercentageAttribute
+case class Liveness(value: Double) extends PercentageAttribute
+case class Speechiness(value: Double) extends PercentageAttribute
+case class Valence(value: Double) extends PercentageAttribute
 
 case class Title(override val value: String) extends TextAttribute
 case class Album(override val value: String) extends TextAttribute
@@ -32,23 +45,9 @@ case class Genre(value: GenreType) extends Attribute
 case class Language(value: LanguageType) extends Attribute
 case class Mood(value: MoodType) extends Attribute
 
-case class Duration(value: Double) extends AudioAttribute {
-  override val max: Double = 1800000
-}
-case class Acousticness(value: Double) extends AudioAttribute
-case class Danceability(value: Double) extends AudioAttribute
-case class Energy(value: Double) extends AudioAttribute
-case class Instrumentalness(value: Double) extends AudioAttribute
-case class Liveness(value: Double) extends AudioAttribute
-case class Loudness(value: Double) extends AudioAttribute {
-  override val min: Double = -60
-  override val max: Double = 0
-}
-case class Speechiness(value: Double) extends AudioAttribute
-case class Tempo(value: Double) extends AudioAttribute {
-  override val max: Double = 240
-}
-case class Valence(value: Double) extends AudioAttribute
+case class Duration(value: Double, min: Double = 0.0, max: Double = 1800000) extends AudioAttribute
+case class Loudness(value: Double, min: Double = -60.0, max: Double = 0.0) extends AudioAttribute
+case class Tempo(value: Double, min: Double = 0.0, max: Double = 240) extends AudioAttribute
 
 case class Time_Signature(value: Int) extends TimeAttribute
 case class Mode(value: Int) extends TimeAttribute
