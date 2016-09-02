640c7ec5875ad2fe9b4cd6684d3f3bd057c33c28
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-02 12:22:11 +0100

    moved parsing to JSONParser

--- app/controllers/PlaylistController.scala
+++ app/controllers/PlaylistController.scala
@@ -2,13 +2,10 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import model.constraints._
-import model.music.{ConstraintsParser, MusicCollection}
-import model.geneticScala.{CostBasedFitness, GA, GAResponse, Playlist}
+import model.music.{JSONParser, MusicCollection}
+import model.genetic.{GA, Playlist}
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, Controller}
-
-import scala.util.parsing.json.JSONArray
 
 /**
   *
@@ -25,65 +22,48 @@ class PlaylistController @Inject() extends Controller {
   in our action code.
    */
   def generatePlaylist =  Action(parse.json) { implicit request =>
-
-    println("received POST request for " + request.body.toString())
-
-    val (name, constraints) = ConstraintsParser.parseRequest(request.body)
-    println("PARSED CONSTRAINTS: =====> " + constraints.toString())
-
-    val ids = ConstraintsParser.parseIDS(request.body)
- //   println("PARSED IDS ========> " + ids.toString())
-
-    // if a constraint with track number is selected, otherwise 20
-    // ALL THIS STUFF SHOULD BE MOVED TO ITS OWN CLASS
-    val n = ConstraintsParser.parseNumberOfTracks(request.body)
-
-    val db = new MusicCollection(ids.flatMap(id => MongoController.readByID(id)))
-
-    // NOT FUNCTIONAL TAKES EXTERNAL VALUES
-    def getPlaylist(ids: Vector[String], c: Set[Constraint]): (Playlist, Option[GAResponse]) = {
-      if (ids.isEmpty) GA.generatePlaylist(new MusicCollection(MongoController.readAll), CostBasedFitness(c), n)
-      else GA.generatePlaylist(db, CostBasedFitness(c), n)
+    println(request.body.toString())
+    JSONParser.parseRequest(request.body) match {
+      case None => BadRequest("Json Request failed")
+      case Some(p) =>
+        println("READY TO ACCESS MONGO")
+        val db = new MusicCollection(p.ids.flatMap(id => MongoController.readByID(id)))
+        println("IDS RETRIEVED FROM MONGO")
+        val (playlist, _) = GA.generatePlaylist(db, p.constraints, p.length)
+        val js = createJsonResponse(p.name, playlist)
+        Ok(js)
     }
+  }
 
-   // val valueConstraints = constraints.filter(c => c.isInstanceOf[MonotonicValue])
-   // val retrievedIDs = getPlaylist(ids, valueConstraints)._1.songs.map(s => s.id)
-
-    /*
-    c.foreach {
-      case x: MonotonicValue => println("FIRST_STEP_BUCKET")
-      case x: MonotonicRange => println("TSA_BUCKET")
-      case _ => println("UNKNOWN_CONSTRAINT")
-    }
-    */
-
-    val (playlist, _) = getPlaylist(ids, constraints.asInstanceOf[Set[Constraint]])
-
-    /*
-    val stats = statistics match {
-      case None => ""
-      case Some(s) => s
-    }
-    */
-
+  def createJsonResponse(name: String, playlist: Playlist): JsValue = {
     val tracksID = Json.toJson(playlist.songs.map(s => s.id))
     val js: JsValue = Json.obj(
       "name" -> name,
       "ids" -> tracksID
-    //  "response" -> statistics.get.distance // TODO proper
+      //  "response" -> statistics.get.distance // TODO
     )
     println("AJAX RESPONSE => " + js.toString())
-    Ok(js)
+    js
   }
 
-    /*
-    (request.body \ "constraints").asOpt[String].map { name =>
-      println("SUCCESSFULLY RETRIEVED JSON " + name)
-      Ok(views.html.index("GEN"))
-    }.getOrElse {
-      BadRequest("Missing parameter")
-    }
-    */
- //   playlist.prettyPrint()
-
 }
+
+
+// val valueConstraints = constraints.filter(c => c.isInstanceOf[MonotonicValue])
+// val retrievedIDs = getPlaylist(ids, valueConstraints)._1.songs.map(s => s.id)
+
+/*
+c.foreach {
+  case x: MonotonicValue => println("FIRST_STEP_BUCKET")
+  case x: MonotonicRange => println("TSA_BUCKET")
+  case _ => println("UNKNOWN_CONSTRAINT")
+}
+*/
+
+
+/*
+val stats = statistics match {
+  case None => ""
+  case Some(s) => s
+}
+*/
