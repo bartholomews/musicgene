490e5bc2812cfb979f2c812d2b47369b5c6443f0
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-04 05:52:19 +0100

    REDIRECT_URI to heroku

--- app/controllers/SpotifyJavaController.java
+++ app/controllers/SpotifyJavaController.java
@@ -30,8 +30,8 @@ public class SpotifyJavaController {
     // TODO inject?
     private static final String CLIENT_ID = "24c87b0353a141768e9b842eb7bd0f67";
     private static final String CLIENT_SECRET = "cc5d6ebca4b445c782b6aced791710ab";
-    private static final String REDIRECT_URI = "http://localhost:9000/callback";
-//    private static final String REDIRECT_URI = "https://musicgene.herokuapp.com/callback";
+//    private static final String REDIRECT_URI = "http://localhost:9000/callback";
+    private static final String REDIRECT_URI = "https://musicgene.herokuapp.com/callback";
 
     private SpotifyJavaController() {}
 
