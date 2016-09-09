535f0e7e84808c588354c45aaee71318d6cbc04b
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-09 03:38:01 +0100

    Include/Exclude Any/All tests

--- test/ConstraintsSpec.scala
+++ test/ConstraintsSpec.scala
@@ -6,9 +6,90 @@ import org.scalatest.{FlatSpec, Matchers}
 /**
   *
   */
+class ConstraintsSpec extends FlatSpec with Matchers {
+  val p1 = new Playlist(Vector(
+    Song("id1", Set(Title("Title_1"), Energy(1.0), Tempo(140))),
+    Song("id2", Set(Title("Title_2"), Energy(1.0), Tempo(140))),
+    Song("id3", Set(Title("Title_3"), Energy(0.5), Tempo(140))),
+    Song("id4", Set(Title("Title_4"), Energy(0.2), Tempo(140))),
+    Song("id5", Set(Title("Title_5"), Tempo(140)))
+  ), CostBasedFitness(Set()))
+
+  //====================================================================================================================
+
+  "An IncludeAny constraint" should "return one score matched if a playlist include that Attribute" in {
+    val score = IncludeAny(Title("Title_1")).score(p1)
+    score.size shouldBe 1
+    score.head.matched shouldBe true
+  }
+
+  it should "return false if a playlist not include that Attribute" in {
+    IncludeAny(Danceability(1.0)).score(p1).head.matched shouldBe false
+  }
+
+  it should "return false if a playlist include that Attribute with different value" in {
+    IncludeAny(Title("Title_0")).score(p1).head.matched shouldBe false
+  }
+
+  //====================================================================================================================
+
+  "An IncludeAll constraint" should "return one score matched if all songs include that Attribute" in {
+    IncludeAll(Tempo(140)).score(p1).head.matched shouldBe true
+  }
+
+  it should "return false if no songs include that Attribute with that value" in {
+    IncludeAll(Danceability(1.0)).score(p1).head.matched shouldBe false
+  }
+
+  it should "return false if only some songs include that Attribute with that value" in {
+    IncludeAll(Energy(1.0)).score(p1).head.matched shouldBe false
+  }
+
+  //====================================================================================================================
+
+  "An ExcludeAny constraint" should "return one score matched if one track does not include that Attribute" in {
+    ExcludeAny(Energy(1.0)).score(p1).head.matched shouldBe true
+  }
+
+  it should "return true if all tracks do not include that Attribute with same value" in {
+    ExcludeAny(Danceability(1.0)).score(p1).head.matched shouldBe true
+  }
+
+  it should "return false if all tracks include that Attribute with same value" in {
+    ExcludeAny(Tempo(140)).score(p1).head.matched shouldBe false
+  }
+
+  it should "return true if all tracks include that Attribute with different value" in {
+    ExcludeAny(Title("Some other title")).score(p1).head.matched shouldBe true
+  }
+
+  //====================================================================================================================
+
+  "An ExcludeAll constraint" should "return one score matched if all tracks do not include that Attribute" in {
+    ExcludeAll(Danceability(1.0)).score(p1).head.matched shouldBe true
+  }
+
+  it should "return true if all tracks include that Attribute with different value" in {
+    ExcludeAll(Title("Some other title")).score(p1).head.matched shouldBe true
+  }
+
+  it should "return false if all tracks include that Attribute with same value" in {
+    ExcludeAll(Tempo(140)).score(p1).head.matched shouldBe false
+
+  }
+
+  it should "return false if some tracks include that Attribute with same value" in {
+    ExcludeAll(Energy(1.0)).score(p1).head.matched shouldBe false
+  }
+
+  //====================================================================================================================
+
+
+
+}
+
 
 /*
-class ConstraintsSpec extends FlatSpec with Matchers {
 
   "A Range Constraint with indexes out of bounds" should "throw exception or do something else" in {
     val c1 = ConstantRange(0, 2, Loudness(10))
@@ -292,68 +373,6 @@ class ConstraintsSpec extends FlatSpec with Matchers {
   }
 
 }
-
-  // =======================================================================================
-
-  //"A UnaryRange constraint" should "work" in {
-
-    //val s1 = new Song("song1", Set(Title("Title"), Loudness(0.2), Mood(Happy)))
-
-    // val c: Constraint = UnaryRange(1, 1.0, 2.0)
-  //  val constraints = Set(UnaryRange)
-   // val ff = StandardFitness(constraints)
- //   val p: Playlist = new Playlist(Seq(s1), new StandardFitness())
-  //  s1.find(Loudness(0.2)) shouldBe Some(Loudness(0.2))
-  //  UnaryRange[Loudness](1, 1.0, 2.0)
-
-  /*
-  val s1 = new Song("song1", Set(Title("Title"), Year(1999), Mood(Happy)))
-  val s2 = new Song("song2", Set(Title("S2"), Year(2000), Mood(Happy)))
-  val s3 = new Song("song3", Set(Title("S3"), Year(2000), Mood(Sad)))
-  val p1 = new Playlist(Seq(s1, s2, s3))
-  val p2 = new Playlist(Seq(s2, s3, s1))
-  val p3 = new Playlist(Seq(s3, s1, s2))
-  */
-
-  /*
-  "An UnaryInclude constraint" should "return false for non-matching attribute" in {
-    val para = new ParameterConstraint(p1)
-    para.UnaryInclude(1, Year(1999)) shouldBe false
-    para.UnaryInclude(1, Year(1999)) shouldBe false
-    para.UnaryInclude(1, Year(2000)) shouldBe true
-    para.UnaryInclude(1, Title("Wrong_Title")) shouldBe false
-    para.UnaryInclude(1, Title("Title")) shouldBe false
-    para.UnaryInclude(1, Title("S2")) shouldBe true
-    para.UnaryInclude(1, Genre(Rock)) shouldBe false
-    para.UnaryInclude(0, Mood(Sad)) shouldBe false
-    para.UnaryInclude(0, Mood(Happy)) shouldBe true
-    para.UnaryInclude(2, Mood(Sad)) shouldBe true
-    para.UnaryInclude(2, Mood(Happy)) shouldBe false
-    para.UnaryInclude(3, Mood(Happy)) shouldBe false
-  }
-  */
-
-  /*
-  it should "do" in {
-    val theArray = List(
-      "6yJJemhHrEX4FZk48D5sAc",
-      "6AedV7UWNkUWyOgggA6old",
-      "5ULcHWDMA3GKd9Ib4pUSh6",
-      "7EipTiVLdacMPjyHs7YF4H",
-      "2oerq776NlrwIqlZnLiyab",
-      "50SqUUqiHAQZjYoOms3FWY",
-      "7JbFBN8rDVzGGxPOuvLEmR",
-      "4SQzIk8jUHYK00sJPoythF",
-      "29MpHvuhoQw30kBgsPGxsY",
-      "4rgHVLNHpDo7p5rii2UMiL",
-      "7jCDWdGx2YgEQrE5onvMRl",
-      "3f5d8ypmJYDe9rGR8pMcfb",
-      "1HIfbJwQRSsmaHyLuxjiu7",
-      "2YPa27G9A8Kkv7MFZUMTyB",
-      "6NOHmRcPMIk7XQBYXavu1a")
-    assert(theArray.forall(s => s.length == 22))
-  }
-*/
 
 
 
