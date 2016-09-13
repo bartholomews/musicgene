45fb3cbcb37bd7cc28a05fe8e4ee4e2cb7ab4d72
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-13 05:33:48 +0100

    renamed generatePlaylists to shuffleAndTake

--- app/model/genetic/PlaylistsFactory.scala
+++ app/model/genetic/PlaylistsFactory.scala
@@ -1,65 +1,34 @@
 package model.genetic
 
-import model.music.MusicCollection
+import model.music.{MusicCollection, Song}
+
+import scala.annotation.tailrec
 
 /**
-  * ALL THESE CONVERSIONS....
-  * ARRAY vs LIST
-  * Random access
-  * That sorting looks expensive
+  *
   */
 object PlaylistsFactory {
 
   def generateUniquePlaylists(db: MusicCollection, poolSize: Int, size: Int, f: FitnessFunction) = {
-    val p = new MusicCollection(generatePlaylist(db, size, f).songs)
-    (for(n <- 1 to poolSize) yield { generatePlaylist(p, size, f) }).toVector
+    val p = new MusicCollection(shuffleAndTake(db, size, f).songs)
+    (for(n <- 1 to poolSize) yield { shuffleAndTake(p, size, f) }).toVector
   }
 
   // Generate 'poolSize` playlists each containing `size` songs from the db collection in random order
   def generatePlaylists(db: MusicCollection, poolSize: Int, length: Int, f: FitnessFunction): Vector[Playlist] = {
-    (for(n <- 1 to poolSize) yield { generatePlaylist(db, length, f) }).toVector
+    (for(n <- 1 to poolSize) yield { shuffleAndTake(db, length, f) }).toVector
   }
-
-  // Generate `poolSize` playlists each containing the whole database collection in random order
-  def generatePlaylists(db: MusicCollection, poolSize: Int, f: FitnessFunction): Vector[Playlist] = {
-    (for (n <- 1 to poolSize) yield { generatePlaylist(db, f) }).toVector
-  }
-
-  def generatePlaylist(db: MusicCollection, size: Int, f: FitnessFunction): Playlist = {
-    new Playlist(scala.util.Random.shuffle(db.songs.distinct).take(size), f)
-  }
-
-  // playlist with the whole collection, removed duplicates
-  def generatePlaylist(db: MusicCollection, f: FitnessFunction): Playlist = {
-    new Playlist(scala.util.Random.shuffle(db.songs).distinct, f)
-  }
-
-  /*  ====================================================================================================
 
   /**
-    * Create a random `Chromosome`.
-    * The `Chromosome` is a candidate playlist encoded as a class with
-    * an Array of genes `Song` IDs, from the pool of `MusicCollection`.
+    * If DB is large, this method will be too slow, better to generate randomInt
     *
-    * @param size the number of the Chromosome's genes (songs in the candidate playlist)
-    * @return a newly generated Chromosome (candidate playlist)
+    * @param db
+    * @param size
+    * @param f
+    * @return
     */
-  def generateChromosome(db: MusicCollection, size: Int): Chromosome = {
-    val genes: Array[String] = util.Random.shuffle(db.IDs).take(size).toArray
-    new Chromosome(genes)
+  def shuffleAndTake(db: MusicCollection, size: Int, f: FitnessFunction): Playlist = {
+    new Playlist(scala.util.Random.shuffle(db.songs.distinct).take(size), f)
   }
-
-  def generateChromosomes(db: MusicCollection, poolSize: Int, size: Int): Array[Chromosome] = {
-    (for(n <- 1 to poolSize) yield { generateChromosome(db, size) }).toArray
-  }
-
-  // preprocessing version, for faster performance to quickly filter out totally irrelevant songs.
-  def generateChromosome(db: MusicCollection, size: Int, p: Boolean): Chromosome = {
-    val filteredCollection: MusicCollection = db.filter(p)
-    val genes: Array[String] = util.Random.shuffle(filteredCollection.IDs).take(size).toArray
-    new Chromosome(genes)
-  }
-
-  */
 
 }
