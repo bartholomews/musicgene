41511aa85f9da21f44d27961fbaade7b6a324b69
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-15 04:22:35 +0100

    cleaned up comments

--- app/controllers/PlaylistController.scala
+++ app/controllers/PlaylistController.scala
@@ -14,38 +14,32 @@ import play.api.mvc.{Action, Controller}
 class PlaylistController @Inject() extends Controller {
 
   // @see https://www.playframework.com/documentation/2.0/ScalaJsonRequests
-  /*
-  The reason why it’s not an Option is because the json body parser
-  will validate that the request has a Content-Type of application/json,
-  and send back a 415 Unsupported Media Type response if the request
-  doesn't meet that expectation. Hence we don’t need to check again
-  in our action code.
-   */
   def generatePlaylist = Action(parse.json) { implicit request =>
     println(request.body.toString())
     JSONParser.parseRequest(request.body) match {
       case None => BadRequest("Json Request failed")
       case Some(p) =>
-        println("READY TO ACCESS MONGO")
         val db = new MusicCollection(
           p.ids.flatMap(id => MongoController.readByID(id))
         )
-        println("IDS RETRIEVED FROM MONGO")
         val (playlist, _) = GA.generatePlaylist(db, p.constraints, p.length)
         val js = createJsonResponse(p.name, playlist)
         Ok(js)
     }
   }
 
+  /**
+    * The JSON response to send back to the user
+    * @param name the name of the playlist
+    * @param playlist the Playlist generated
+    * @return a JSON with the name of the playlist and an array of tracks ids
+    */
   def createJsonResponse(name: String, playlist: Playlist): JsValue = {
     val tracksID = Json.toJson(playlist.songs.map(s => s.id))
-    val js: JsValue = Json.obj(
+    Json.obj(
       "name" -> name,
       "ids" -> tracksID
-      //  "response" -> statistics.get.distance // TODO
     )
-    println("AJAX RESPONSE => " + js.toString())
-    js
   }
 
 }
--- app/controllers/SpotifyController.scala
+++ app/controllers/SpotifyController.scala
@@ -3,22 +3,16 @@ package controllers
 import javax.inject._
 
 import com.wrapper.spotify.exceptions.BadRequestException
-import com.wrapper.spotify.models.{AudioFeature, SimplePlaylist, Track}
+import com.wrapper.spotify.models.{SimplePlaylist, Track}
 import model.music._
-//import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc._
 
 import scala.collection.JavaConversions._
-import collection.JavaConverters._
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
+ * This controller redirects the user to authenticate with Spotify,
+  * then retrieves the tracks from the API if not already in MongoDB
   *
-  * TODO should retrieve all IDs single call,
-  * if not in the cache then retrieve the audioAnalysis
  */
 @Singleton
 class SpotifyController @Inject() extends Controller {
@@ -39,48 +33,24 @@ class SpotifyController @Inject() extends Controller {
         Ok(views.html.callback("Welcome, " + spotify.getName))
       case None => BadRequest("Something went wrong. Please go back and try again.")
     }
-    /**
-      * TODO
-      * is badRequest a proper return code for each?
-      * if VALUES: [access_denied] return badRequest(index.render(...));
-      * else (if VALUES: [invalid_scope] or something else)
-      * return badRequest(callback.render(map.get("error")[0]));
-      */
   }
 
   def getSpotifyName: String = spotify.getName
 
   def getPlaylists: Vector[(String, Vector[Song])] = {
     val simplePlaylists: Vector[SimplePlaylist] = spotify.getSavedPlaylists.toVector
-    System.out.println("Playlists retrieved")
     val playlists: Vector[(SimplePlaylist, Vector[Song])] = getPlaylistsCollection(simplePlaylists)
     playlists.map(v => (v._1.getName, v._2))
   }
 
   def getPlaylistsCollection(list: Vector[SimplePlaylist]): Vector[(SimplePlaylist, Vector[Song])] = {
     list.flatMap(p => getPlaylistCollection(p))
-    /*
-    val tuple: Vector[(SimplePlaylist, Vector[Track])] = getPlaylistTuple(list)
-    tuple.map(t => (t._1, getTracksFeatures(t._2)))
-  */
   }
 
-  /*
-  def getPlaylistsCollection(list: List[SimplePlaylist]): List[(SimplePlaylist, List[Song])] = {
-    val tuple: List[(SimplePlaylist, List[Track])] = getPlaylistTuple(list)
-    tuple.map(t => (t._1, getTracksFeatures(t._2)))
-  }
-  */
-
-  // TODO rate limits...
   def getPlaylistCollection(playlist: SimplePlaylist): Option[(SimplePlaylist, Vector[Song])] = {
-    println("creating playlist collection..")
     try {
       val trackList: Vector[Track] = spotify.getPlaylistTracks(playlist).map(t => t.getTrack).toVector
-      println("RETRIEVED TRACKLIST")
-
       val inDB: Vector[Song] = trackList.flatMap(t => MongoController.readByID(t.getId))
-
       val outDB: Vector[Song] = MusicUtil.toSongs(
         trackList.filterNot(t => inDB.exists(s => s.id == t.getId))
           .flatMap(t => spotify.getAnalysis(t.getId) match {
@@ -88,15 +58,11 @@ class SpotifyController @Inject() extends Controller {
             case Some(analysis) => Some(t, analysis)
           }
       ))
-
-      println(outDB.size + " songs fetched from Spotify API")
-      // TODO SHOULD BE ASYNC
       MongoController.writeToDB(outDB)
-      // TODO sort
       Some(playlist, inDB ++ outDB)
     } catch {
       case _: NullPointerException => None
-      case _: BadRequestException => None // TODO handle properly
+      case _: BadRequestException => None
     }
   }
 
@@ -110,26 +76,6 @@ class SpotifyController @Inject() extends Controller {
     playlists.map(p => (p, spotify.getPlaylistTracks(p).map(t => t.getTrack).toVector))
   }
 
-  // this makes sense and you should modify the wrapper to do multiple requests at one time
   def getIDs(tracks: List[Track]): String = tracks.map(t => t.getId).mkString(",")
-
-  /*
-  def getTracksAnalysis(tracks: List[Track]) = {
-    val code = "BQAHIwOOnWpUS_amq7ZSjgHglMdCOikKmmRnLrJJeECaLFWHvUeqvR82j7p17kc5m8ClU6ekGgJiOybgLd7vsH5fH8"
-    val wsURL = ws.url("https://api.spotify.com/v1/audio-features")
-    val ids = tracks.map(t => t.getId).mkString(",")
-    val bear = "Bearer " + code
-
-    val req: WSRequest =
-      wsURL.withHeaders("Authorization" -> bear)
-        .withQueryString("ids" -> ids)
-
-    println(req.toString)
-
-    val result = req.get().map {
-      //  response => (response.json \ "danceability").as[String]re
-      response => response.allHeaders.toString()
-    }
-    */
 
 }
