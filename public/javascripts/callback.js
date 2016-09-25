74be0541cb0eaad8ba352ed6a7440f059085fedf
BBK-PiJ-2014-21 <fbarto01@mail.bbk.ac.uk>
2016-09-25 04:13:05 +0100

    added waiting para and background

---
+++ public/javascripts/callback.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param p
+ */
+function addWaitingPara(p) {
+    var para = document.getElementById(p);
+    para.innerHTML = "Please wait, it might take a few seconds. ";
+    var span = document.createElement('span');
+    span.className = 'fa fa-2x fa-spin fa-cog';
+    para.appendChild(span);
+}
