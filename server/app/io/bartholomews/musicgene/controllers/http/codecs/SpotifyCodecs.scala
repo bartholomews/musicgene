552c35a8fe99d62a3682a3935120289513892448
bartholomews <github@bartholomews.io>
2020-09-13 13:45:44 +0100

    Remove duplicate packages

--- server/app/io/bartholomews/musicgene/controllers/http/codecs/SpotifyCodecs.scala
+++
@@ -1,8 +0,0 @@
-package io.bartholomews.musicgene.controllers.http.codecs
-
-import io.bartholomews.spotify4s.entities.SpotifyId
-import play.api.libs.json.{Format, Json}
-
-object SpotifyCodecs {
-  implicit val spotifyIdFormat: Format[SpotifyId] = Json.valueFormat[SpotifyId]
-}
--- server/app/io/bartholomews/musicgene/model/music/AudioTrack.scala
+++
@@ -1,6 +0,0 @@
-package io.bartholomews.musicgene.model.music
-
-/**
-  *
-  */
-case class AudioTrack(id: String, attributes: Set[Attribute[_]])
